83f8093743e40a42046a3beaab32236c52d9efd8
Økame <ensokame@gmail.com>
2017-09-17 13:20:03 +0600

    Create colorpicker.js

---
+++ plugins/colorpicker.js
@@ -0,0 +1,8 @@
+/*Подключаем сторонние ресурсы*/
+document.write('<link href="https://jqwidgets.com/public/jqwidgets/styles/jqx.energyblue.css" rel="stylesheet">');
+document.write('<link href="https://jqwidgets.com/public/jqwidgets/styles/jqx.base.css" rel="stylesheet">');
+/*Рисуем*/
+$("#colorPicker").jqxColorPicker({
+    width: 195,
+    height: 195
+});
